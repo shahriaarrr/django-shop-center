8383f82db2c95f9c5f6a506d5c914ee02453f43f
Yasirooz <testwebsite1244@gmail.com>
2021-07-12 16:43:11 +0430

    change setting files settings.py and urls.py in shop folder for static files, make folders js, css and image on staticfiles and create files base.css, login.css, register.css and home.css for style pages home,register,login and base nabvar

--- shop/settings.py
+++ shop/settings.py
@@ -120,6 +120,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles')
+]
+
+STATIC_ROOT = os.path.join(
+    BASE_DIR,'static'
+    )
+
+# -----------------
+# media
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- shop/urls.py
+++ shop/urls.py
@@ -16,8 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+# imports for staticfiles
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('Shoping.urls'))
 ]
+
+# shart baraye deploy kardan
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,documen_root=settings.MEDIA_ROOT)
