0aa5628314de3924804fea5129030ab222433858
Yasirooz <testwebsite1244@gmail.com>
2021-07-12 17:58:33 +0430

    Update

--- Shoping/models.py
+++ Shoping/models.py
@@ -5,3 +5,7 @@ class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     price = models.DecimalField(max_digits=20,decimal_places=2)
+
+
+    def __str__(self):
+        return self.title
