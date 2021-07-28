13ab7ff21ffff0a8b23412d8929b9e842d7492ff
Yasirooz <testwebsite1244@gmail.com>
2021-07-28 08:42:03 +0430

    Update

--- Shoping/views.py
+++ Shoping/views.py
@@ -240,23 +240,20 @@ def AboutPage(request):
 
 #---- Order
 # zamani mishe kharid kard ke user login bashe
-# @login_required
+@login_required(login_url='Shoping:login')
 def add_user_order(request):
-    if request.user.is_authenticated:
-        new_order_form = UserNewOrderForm(request.POST or None)
+    new_order_form = UserNewOrderForm(request.POST or None)
 
-        if new_order_form.is_valid():
-            order = Order.objects.filter(owner_id=request.user.id, is_paid=False).first()
-            if order is None:
-                order = Order.objects.create(owner_id=request.user.id,is_paid=False)
-            product_id = new_order_form.cleaned_data.get('product_id')
-            count = new_order_form.cleaned_data.get('count')
-            if count < 0:
-                count = 1
-            product = Product.objects.get_queryset().filter(id=product_id).first()
-            order.orderdetail_set.create(product_id=product.id,price=product.price, count=count)
+    if new_order_form.is_valid():
+        order = Order.objects.filter(owner_id=request.user.id, is_paid=False).first()
+        if order is None:
+            order = Order.objects.create(owner_id=request.user.id,is_paid=False)
+        product_id = new_order_form.cleaned_data.get('product_id')
+        count = new_order_form.cleaned_data.get('count')
+        if count < 0:
+            count = 1
+        product = Product.objects.get_queryset().filter(id=product_id).first()
+        order.orderdetail_set.create(product_id=product.id,price=product.price, count=count)
             
         
         return redirect('Shoping:home')
-    else:
-        return redirect('Shoping:login')
